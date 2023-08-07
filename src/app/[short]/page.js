0d225b6f6598a8f668319271f689a5ec58b40d2a
Muhammad Bilal <muhammad.bilal.94390@gmail.com>
2023-08-07 12:24:23 +0500

    Added Page Redirect

--- src/app/[short]/page.js
+++ src/app/[short]/page.js
@@ -1,14 +1,24 @@
-import {notFound} from 'next/navigation'
+import {notFound,redirect} from 'next/navigation'
 import { getShortUrlRecord } from "../lib/db"
 
 export default async function ShortPage({params}){
     const {short} = params
     const [record] = await getShortUrlRecord(short)
+    const {url} = record
     if(!record){
         notFound()
     }
-    return <>
     
-            {JSON.stringify(record)}
-    </>
+    if(!short){
+        notFound()
+    }
+
+    
+    if(!url){
+        notFound()
+    }
+    // return <>
+    //         {JSON.stringify(record)}
+    // </>
+    redirect(url,"push")
 }
