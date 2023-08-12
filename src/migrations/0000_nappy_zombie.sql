f566c8f35d06083ca8b07d6d726328891040d6b0
Muhammad Bilal <muhammad.bilal.94390@gmail.com>
2023-08-12 15:34:21 +0500

    Final Commit

--- src/migrations/0000_nappy_zombie.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "links" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"url" text NOT NULL,
-	"short" varchar,
-	"createdAt" timestamp DEFAULT now()
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX IF NOT EXISTS "url_idx" ON "links" ("url");
